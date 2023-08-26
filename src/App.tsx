05e20bc8e7ee93cef0131ed42a0ebb768f0a0b2f
fredachang <f.chang122@gmail.com>
2023-08-27 00:02:36 +1000

    added p5

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import "./App.css";
 import { BasicCanvas } from "./components/BasicCanvas";
+import { P5Canvas } from "./components/p5";
 import { drawArt } from "./utils/drawArt";
 
 function App() {
   return (
     <>
-      <section className="main-section">
-        <BasicCanvas
+      <section className="w-screen h-screen flex">
+        <div className="z-20">
+          <P5Canvas />
+        </div>
+        <div className="w-screen h-screen absolute top-0">
+          <img src="/star.png" className="w-full h-full object-cover" />
+        </div>
+        {/* <BasicCanvas
           draw={drawArt}
           width={window.innerWidth}
           height={window.innerHeight}
-        />
+        /> */}
         {/* <BasicCanvas
           draw={moveCircle}
           width={window.innerWidth}
---
+++ src/components/p5.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Sketch from "react-p5";
+import p5Types from "p5"; //Import this for typechecking and intellisense
+
+interface ComponentProps {
+  // Your component props
+}
+
+export const P5Canvas: React.FC<ComponentProps> = (props: ComponentProps) => {
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    //defines the resolution of the graphics, higher the more refined
+    p5.pixelDensity(10);
+
+    //draw function automatically runs at 60 frames per second
+    //you can change it to something else using the frameRate function
+    p5.frameRate(60);
+
+    //canvas size. P5 has built in windowWidth and windowHeight
+    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+
+    //fills the bakcground with a background colour.
+    //takes an integer value between 0 and 255 , 0 is pure black, and 255 is pure white.
+  };
+
+  //   const draw = (p5: p5Types) => {
+  //     p5.background(0);
+  //     p5.ellipse(x, y, 70, 70);
+  //     x++;
+  //   };
+
+  function draw(p5: p5Types) {
+    //use clear to clear the previous frames.
+    //Normally would call background(0) but that would mean assigning it a colour and not being able to see the reference pic
+    p5.clear();
+    showCoordinateWithMouse(p5);
+    p5.noFill();
+    // p5.stroke(255, 102, 0);
+    // p5.line(85, 20, 10, 10);
+    // p5.line(90, 90, 15, 80);
+    p5.stroke(0, 0, 0);
+    p5.strokeWeight(2);
+    p5.beginShape();
+    p5.vertex(30, 180);
+    p5.bezierVertex(472, 408, 360, 87, 470, 113);
+    p5.endShape();
+  }
+
+  function showCoordinateWithMouse(p5: p5Types) {
+    p5.fill("Salmon");
+    p5.textSize(16);
+    p5.textAlign(p5.CENTER);
+    p5.text(
+      "(" + p5.floor(p5.mouseX) + ", " + p5.floor(p5.mouseY) + ")",
+      p5.mouseX,
+      p5.mouseY
+    );
+  }
+
+  return <Sketch setup={setup} draw={draw} />;
+};
